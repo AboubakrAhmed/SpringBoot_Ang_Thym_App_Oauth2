16f12fa4cccd8f2483412f32ca4b72619640882d
aboubakrahmprj <aboubakrahmprj@gmail.com>
2024-07-29 16:09:05 +0200

    Conflict resolution : Delete unnecessary files + Rename Files after pulling from github

--- src/main/java/com/example/customerfrontthyemleafapp/reporsitory/CustomerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.customerfrontthyemleafapp.reporsitory;
-
-import com.example.customerfrontthyemleafapp.entities.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer,Long> {
-
-}
