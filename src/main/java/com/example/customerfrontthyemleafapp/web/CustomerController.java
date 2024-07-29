5a65af3e41797685032870bb87c98b9ae90e3644
aboubakrahmprj <aboubakrahmprj@gmail.com>
2024-07-29 11:22:19 +0200

    Add KeyCloak Authentication configuration + Update Security config and Web CustomerControler to manage Roles [Roles Mapping]

--- src/main/java/com/example/customerfrontthyemleafapp/web/CustomerController.java
+++ src/main/java/com/example/customerfrontthyemleafapp/web/CustomerController.java
@@ -2,14 +2,28 @@ package com.example.customerfrontthyemleafapp.web;
 
 
 import com.example.customerfrontthyemleafapp.entities.Customer;
-import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+
+
 import org.springframework.ui.Model;
 import com.example.customerfrontthyemleafapp.reporsitory.CustomerRepository;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestClient;
 
-import java.net.Authenticator;
+
+
 import java.util.List;
 
 @Controller
@@ -21,6 +35,7 @@ public class CustomerController {
         this.customerRepository = customerRepository;
     }
     @GetMapping("/customers")
+    @PreAuthorize("hasAnyAuthority('ADMIN')")
     public String customers(Model model){
         List<Customer> customerList = customerRepository.findAll();
         model.addAttribute("customer",customerList);
@@ -32,12 +47,16 @@ public class CustomerController {
     }
     @GetMapping("/auth")
     @ResponseBody
-    public Authentication authentication(Authentication authentication){
+    public Authentication authentication(Authentication authentication) {
         return authentication;
     }
     @GetMapping("/")
     public String index(){
         return "index";
+    }
+    @GetMapping("/notAuthorized")
+    public String notAuthorized(){
+        return "notAuthorized";
     }
 
 
